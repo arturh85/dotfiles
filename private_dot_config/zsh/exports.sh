20735ff55879f7274129e784538e9c8727acf5a6
Artur Hallmann <arturh@arturh.de>
2021-11-06 21:37:37 +0100

    windows fixes

--- private_dot_config/zsh/exports.sh
+++ private_dot_config/zsh/exports.sh
@@ -2,6 +2,7 @@ export PATH="$PATH:$HOME/.cargo/bin"
 export PATH="/snap/bin:$PATH"
 export EXA_COLORS="da=1;34"
 export EDITOR="nvim"
+export GPG_TTY=$(tty)
 
 # set PATH so it includes user's private bin if it exists
 if [ -d "$HOME/bin" ] ; then
