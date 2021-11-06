7b982b55a6cc36cbc81fabc748cada73ab755bfc
Artur Hallmann <arturh@arturh.de>
2021-11-06 01:15:16 +0100

    add private bin to PATH

--- private_dot_bash_exports.sh
+++ private_dot_bash_exports.sh
@@ -2,3 +2,8 @@ export PATH="$PATH:$HOME/.cargo/bin"
 export PATH="/snap/bin:$PATH"
 export EXA_COLORS="da=1;34"
 export EDITOR="nvim"
+
+# set PATH so it includes user's private bin if it exists
+if [ -d "$HOME/bin" ] ; then
+    export PATH="$HOME/bin:$PATH"
+fi
